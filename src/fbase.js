395966b2f704fd4a781630ad1b7d52a575194e15
21people <dnjsaud0427@naver.com>
2021-05-29 13:57:31 +0900

    1.2 Router

---
+++ src/fbase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+// Key를 GitHube에 업로드하기 싫어서 .env 만들고 대체함.
+
+export default firebase.initializeApp(firebaseConfig);
